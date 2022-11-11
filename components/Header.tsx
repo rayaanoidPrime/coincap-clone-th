a641417aa9b6d9e1daf9fd336c2985e0236a342e
rayaanoidPrime <rayaanghosh18@gmail.com>
2022-11-11 23:46:07 +0530

    header + server init

---
+++ components/Header.tsx
@@ -0,0 +1,91 @@
+import React, { FC } from "react";
+import Image from "next/image";
+import Search from '../assets/svg/search'
+const styles = {
+    header: `bg-[#17171A] text-white h-20 flex gap-[100px] w-full p-[30px]`,
+    headerWrapper: `flex justify-center h-full max-w-screen-xl mx-auto px-4`,
+    nav: `flex justify-center items-center gap-[20px] `,
+    navItem: `relative mr-2 cursor-pointer hover:opacity-60`,
+    navLinks: ``,
+    badge: `rounded-full bg-blue-600 h-1 w-1 bottom-5 absolute right-0 top-1 ring-4`,
+    inputContainer: `flex inputContainer p-2 rounded bg-[#17124]`,
+    input: `bg-transparent outline-none text-white w-70  ml-3`
+}
+
+const Header: FC = () => {
+    return (
+        <div className={styles.header}>
+            <Image src='https://s2.coinmarketcap.com/static/cloud/img/coinmarketcap_white_1.svg'
+                alt="logo"
+                width={200}
+                height={200} />
+
+            <div className={styles.headerWrapper}>
+                <nav className={styles.nav}>
+                    <div className={styles.navItem}>
+                        <div className={styles.navLinks}>
+                            Cryptocurrencies
+                        </div>
+                        <div className={styles.badge} />
+                    </div>
+
+                    <div className={styles.navItem}>
+                        <div className={styles.navLinks}>
+                            Exchanges
+                        </div>
+                    </div>
+
+                    <div className={styles.navItem}>
+                        <div className={styles.navLinks}>
+                            NFTs
+                        </div>
+                        <div className={styles.badge} />
+                    </div>
+
+                    <div className={styles.navItem}>
+                        <div className={styles.navLinks}>
+                            Cryptown
+                        </div>
+                        <div className={styles.badge} />
+                    </div>
+
+                    <div className={styles.navItem}>
+                        <div className={styles.navLinks}>
+                            Portfolio
+                        </div>
+                    </div>
+
+                    <div className={styles.navItem}>
+                        <div className={styles.navLinks}>
+                            Watchlist
+                        </div>
+                    </div>
+
+                    <div className={styles.navItem}>
+                        <div className={styles.navLinks}>
+                            Products
+                        </div>
+                        <div className={styles.badge} />
+                    </div>
+
+                    <div className={styles.navItem}>
+                        <div className={styles.navLinks}>
+                            Learn
+                        </div>
+                    </div>
+
+                </nav>
+
+                <div className="flex items-center">
+                    {/* <ConnectButton /> */}
+                    <div className={styles.inputContainer}>
+                        <Search />
+                        <input className={styles.input} placeholder="Search"></input>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MoralisProvider } from 'react-moralis'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      serverUrl={process.env.NEXT_PUBLIC_REACT_APP_SERVER_URL ?? ''}
+      appId={process.env.NEXT_PUBLIC_REACT_APP_APPLICATION_ID ?? ''}
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  )
 }
-
 export default MyApp
